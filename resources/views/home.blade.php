94ecfa6f00f2682ff457436dacadf0f833c174e2
AceFM6 <f.martin@groupe-inedis.fr>
2020-12-21 17:07:20 +0100

    suppression du surplus jetstream

---
+++ resources/views/home.blade.php
@@ -0,0 +1,8 @@
+<x-app-layout>
+    <div class="py-12">
+        <div class="max-w-full mx-auto sm:px-6 lg:px-10">
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+            </div>
+        </div>
+    </div>
+</x-app-layout>
