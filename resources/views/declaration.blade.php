f5f770feaff11336adf6f61f1489c39812c44250
AceFM6 <f.martin@groupe-inedis.fr>
2021-01-14 11:25:37 +0100

    Ajout de commentaire dans la vue déclaration

--- resources/views/declaration.blade.php
+++ resources/views/declaration.blade.php
@@ -1,10 +1,29 @@
 <x-app-layout>
     <div class="py-4 sm:py-6 lg:py-10">
         <div class="max-w-full mx-auto sm:px-6 lg:px-10">
-            @foreach (Auth::user()->contrats as $contrat)
-                {{ $contrat->name }}
-                <br>
-            @endforeach
+            
+            <!-- affichage des contrats de l'utilisateur connecté -->
+            @if (count(Auth::user()->contrats) == 1)
+                1 contrat
+            @elseif (count(Auth::user()->contrats) > 1)
+                plusieurs contrat
+            @else
+                Pas de contrat à déclarer
+            @endif
+            <ul>
+                @foreach (Auth::user()->contrats as $contrat)
+                    <li>
+                        {{ $contrat->id }}
+                        {{ $contrat->name }}
+                    </li>
+                @endforeach
+            </ul>
+
+            <!-- affichage des magasins disponible -->
+
+            
+
+            <!-- affichage des contrats magasins déjà existant -->
 
         </div>
     </div>
